c2ca0c9102bf104f2ddf64f5cbfb8a5ad7c220f9
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-01-27 17:00:51 +0100

    add swagger

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 'use strict';
 
 const Joi = require('joi');
-
+const HapiSwagger = require('hapi-swagger');
+const Inert = require('inert');
+const Vision = require('vision');
 
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 server.connection({port: 9733});
+
+const options = {
+    info: {
+        'title': 'Test API Documentation',
+    }
+};
 
 
 var dbOpts = {
@@ -27,46 +35,60 @@ server.register({
         throw err;
     }
 
+    server.register([
+        Inert,
+        Vision,
+        {
+            'register': HapiSwagger,
+            'options': options
+        }], (err) => {
 
-    server.route({
-        method: 'POST',
-        path: '/money/location',
-        handler: (request, reply) => {
-
-            var db = request.server.plugins['hapi-mongodb'].db;
-            var money = db.collection('money');
-
-            money.insertOne(request.payload);
-
-            reply('OK');
-        },
-        config: {
-            validate: {
-                payload: {
-                    long: Joi.string().required(),
-                    lat: Joi.string().required()
-                }
-            }
-        }
-    });
-
-    server.route({
-        method: 'GET',
-        path: '/money/location',
-        handler: (request, reply) => {
-            var db = request.server.plugins['hapi-mongodb'].db;
-            var money = db.collection('money');
-
-            money.find().toArray().then(docs => reply(docs)).catch(reply('sorry'));
-        }
-    });
-
-
-    server.start(err => {
         if (err) {
             throw err;
         }
-        console.log('Server running at:', server.info.uri);
+
+
+        server.route({
+            method: 'POST',
+            path: '/money/location',
+            handler: (request, reply) => {
+
+                var db = request.server.plugins['hapi-mongodb'].db;
+                var money = db.collection('money');
+
+                money.insertOne(request.payload);
+
+                reply('OK');
+            },
+            config: {
+                validate: {
+                    payload: {
+                        long: Joi.string().required(),
+                        lat: Joi.string().required()
+                    }
+                }
+            }
+        });
+
+        server.route({
+            method: 'GET',
+            path: '/money/location',
+            handler: (request, reply) => {
+                var db = request.server.plugins['hapi-mongodb'].db;
+                var money = db.collection('money');
+
+                money.find().toArray().then(docs => reply(docs)).catch(reply('sorry'));
+            }
+        });
+
+
+        server.start(err => {
+            if (err) {
+                throw err;
+            }
+            console.log('Server running at:', server.info.uri);
+        });
+
     });
 
 });
